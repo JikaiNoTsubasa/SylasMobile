892826b31bad697a8e771a31a8199723cf60a856
Stéphane Biehler <biehler@s2i-evolution.eu>
2025-05-15 13:37:34 +0200

    ✨ Added capacitor support
    Added capacitor support

---
+++ android/app/src/main/java/fr/coregeek/mobile/MainActivity.java
@@ -0,0 +1,5 @@
+package fr.coregeek.mobile;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'fr.coregeek.mobile',
+  appName: 'SylasMobile',
+  webDir: 'dist/sylas-mobile/browser'
+};
+
+export default config;
